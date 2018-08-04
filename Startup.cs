50e4f31e2409dd6f26711ccd866c8231d0e92f8f
haka2087 <otokrause@gmail.com>
2018-08-04 12:22:30 -0400

    Avance Sql

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace Arq.Scheduler
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
